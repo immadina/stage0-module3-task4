3187bce17129fab507ca40c03ae5e3d1a2cf0c63
madina <91madina@gmail.com>
2022-08-17 14:05:55 +0600

    Module #3, Task #4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a = 5;
+        byte b = 3;
+        int formula = (9 * (a^2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+        System.out.println(formula);
     }
 }
